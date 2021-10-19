150d5ff34403012b39e6dcaeb51ee6de5c9a265e
Erwann Le Gall <erwannj.legall@gmail.com>
2021-10-19 11:33:42 +0200

    3.7: Phonebook backend step7

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { response } = require("express");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
 	{
@@ -92,6 +94,12 @@ app.post("/api/persons", (request, response) => {
 
 	response.json(person);
 });
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
