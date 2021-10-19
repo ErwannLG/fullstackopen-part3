83e237c5f169f2954bc0418e0b6ebbb680ab9d82
Erwann Le Gall <erwannj.legall@gmail.com>
2021-10-19 09:43:53 +0200

    3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/", (request, response) => {
 });
 
 app.get("/info/", (request, response) => {
-	let time = new Date();
+	const time = new Date();
 	response.send(`
 		<p>Phonebook has info for ${persons.length} people</p>
 		<p>${time}</p>
@@ -52,6 +52,13 @@ app.get("/api/persons/:id", (request, response) => {
 	} else {
 		response.status(404).end();
 	}
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+	const id = Number(request.params.id);
+	persons = persons.filter((person) => person.id !== id);
+
+	response.status(204).end();
 });
 
 const PORT = 3001;
