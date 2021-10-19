fc9fae766d57c2d9a3324ef0c0109153344309cf
Erwann Le Gall <erwannj.legall@gmail.com>
2021-10-19 09:58:31 +0200

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -61,6 +61,32 @@ app.delete("/api/persons/:id", (request, response) => {
 	response.status(204).end();
 });
 
+const generateId = () => {
+	const maxId = 100000;
+	const randomId = Math.floor(Math.random() * maxId);
+	return randomId;
+};
+
+app.post("/api/persons", (request, response) => {
+	const body = request.body;
+
+	if (!body.name || !body.number) {
+		return response.status(400).json({
+			error: "name or number missing",
+		});
+	}
+
+	const person = {
+		id: generateId(),
+		name: body.name,
+		number: body.number,
+	};
+
+	persons = persons.concat(person);
+
+	response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
