06e7d7aea20364a0fcaae38c8c606c4218b48700
Erwann Le Gall <erwannj.legall@gmail.com>
2021-10-20 16:25:53 +0200

    3.15 & 3.16: Phonebook database, step3 & step4

--- index.js
+++ index.js
@@ -41,27 +41,24 @@ app.get("/api/persons/", (request, response) => {
 	});
 });
 
-app.get("/api/persons/:id", (request, response) => {
-	// const id = Number(request.params.id);
-	// const person = persons.find((person) => person.id === id);
-
-	// Person.findById(request.params.id)
-
-	// if (person) {
-	// 	response.json(person);
-	// } else {
-	// 	response.status(404).end();
-	// }
-	Person.findById(request.params.id).then((person) => {
-		response.json(person);
-	});
+app.get("/api/persons/:id", (request, response, next) => {
+	Person.findById(request.params.id)
+		.then((person) => {
+			if (person) {
+				response.json(person);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-	const id = Number(request.params.id);
-	persons = persons.filter((person) => person.id !== id);
-
-	response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -88,6 +85,18 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+	console.log(error.message);
+
+	if (error.name === "CastError") {
+		return response.status(400).send({ error: "malformatted id" });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
