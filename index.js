a6a3ad95aa819f3e82872a0bc49f534ad03a7ab0
Erwann Le Gall <erwannj.legall@gmail.com>
2021-10-21 15:21:01 +0200

    3.22: Lint configuration

--- index.js
+++ index.js
@@ -1,133 +1,133 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
-const Person = require("./models/person");
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
-app.use(express.json());
-app.use(express.static("build"));
-app.use(cors());
+app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
-	console.log("Method:", request.method);
-	console.log("Path:  ", request.path);
-	console.log("Body:  ", request.body);
-	console.log("---");
-	next();
-};
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-app.use(requestLogger);
+app.use(requestLogger)
 
-morgan.token("body", function (req, res) {
-	return JSON.stringify(req.body);
-});
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body)
+})
 
 app.use(
-	morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
-app.get("/info/", (request, response) => {
-	const time = new Date();
-	Person.find({}).then((people) => {
-		response.send(`
+app.get('/info/', (request, response) => {
+  const time = new Date()
+  Person.find({}).then((people) => {
+    response.send(`
 			<p>Phonebook has info for ${people.length} people</p>
 			<p>${time}</p>
-		`);
-	});
-});
+		`)
+  })
+})
 
-app.get("/api/persons/", (request, response) => {
-	Person.find({}).then((persons) => {
-		response.json(persons);
-	});
-});
+app.get('/api/persons/', (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
-	Person.findById(request.params.id)
-		.then((person) => {
-			if (person) {
-				response.json(person);
-			} else {
-				response.status(404).end();
-			}
-		})
-		.catch((error) => next(error));
-});
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
-	Person.findByIdAndRemove(request.params.id)
-		.then((result) => {
-			response.status(204).end();
-		})
-		.catch((error) => next(error));
-});
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (request, response, next) => {
-	const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-	if (!body.name || !body.number) {
-		return response.status(400).json({
-			error: "information missing",
-		});
-	}
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'information missing',
+    })
+  }
 
-	const person = new Person({
-		name: body.name,
-		number: body.number,
-	});
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-	person
-		.save()
-		.then((savedPerson) => savedPerson.toJSON())
-		.then((savedAndFormattedPerson) => {
-			response.json(savedAndFormattedPerson);
-		})
-		.catch((error) => next(error));
-});
+  person
+    .save()
+    .then((savedPerson) => savedPerson.toJSON())
+    .then((savedAndFormattedPerson) => {
+      response.json(savedAndFormattedPerson)
+    })
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-	const body = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
-	const person = {
-		name: body.name,
-		number: body.number,
-	};
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-	Person.findByIdAndUpdate(request.params.id, person, {
-		new: true,
-		runValidators: true,
-	})
-		.then((updatedPerson) => {
-			return updatedPerson.toJSON();
-		})
-		.then((updatedAndFormattedPerson) => {
-			response.json(updatedAndFormattedPerson);
-		})
-		.catch((error) => next(error));
-});
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+  })
+    .then((updatedPerson) => {
+      return updatedPerson.toJSON()
+    })
+    .then((updatedAndFormattedPerson) => {
+      response.json(updatedAndFormattedPerson)
+    })
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-	console.log(error.message);
+  console.log(error.message)
 
-	if (error.name === "CastError") {
-		return response.status(400).send({ error: "malformatted id" });
-	} else if (error.name === "ValidationError") {
-		return response.status(400).json({ error: error.message });
-	}
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-	next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
